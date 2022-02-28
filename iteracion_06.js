f685eb9278c4affa74f6f92cb47c6daad6fb88e8
Ruben <ruben.infantes@gmail.com>
2022-02-28 16:10:49 +0100

    iteration#06 complete

---
+++ iteracion_06.js
@@ -0,0 +1,19 @@
+for (let bucle = 0; bucle < 10; bucle++) {
+
+    console.log(bucle);
+}
+
+
+for (let bucle = 0; bucle < 10; bucle++) {
+
+    if (bucle % 2 == 0) {
+        console.log(bucle)
+    }
+}
+
+for (let sheep = 0; sheep < 11; sheep++) {
+    console.log(sheep + " intentando dormir");
+    if (sheep === 10) {
+        console.log("Dormido");
+    }
+}
